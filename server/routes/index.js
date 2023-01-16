e43169a9737e413eb8bb1c5873465719f9b8e5f8
doohyunheo <engus1220@gmail.com>
2023-01-16 19:59:09 +0900

    server, client 분리 및 연동

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var cors = require("cors");
 
+router.use(cors());
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
