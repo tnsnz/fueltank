d4ee626a6d0a0fafe2a5bf2a36813010d72c530d
doohyunheo <engus1220@gmail.com>
2023-01-18 20:09:48 +0900

    ORM 적용

--- server/app.js
+++ server/app.js
@@ -5,7 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
+
+var models = require("./models/index.js");
+
+models.sequelize.sync().then(() => {
+    console.log("DB 연결 성공");
+}).catch(err => {
+    console.log("DB 연결 실패");
+    console.log(err);
+})
 
 var app = express();
 
@@ -15,7 +24,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +32,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var cors = require("cors");
+router.use(cors());
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
