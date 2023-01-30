866a3519b5fbf8cb7fdf6ef926dba03fca2aaab1
tnsnz <tnsnz@github.com>
2023-01-30 16:11:43 +0900

    fix folder structure of node project

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var usersRouter = require('./routes/user');
 
-var models = require("./models/index.js");
+var models = require("./models");
 
 models.sequelize.sync().then(() => {
     console.log("DB 연결 성공");
